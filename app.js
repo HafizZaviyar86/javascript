58ffc04f037687de20eeb77f0bdce5429a68f473
HafizZaviyar86 <zaviyarsaleem86@gmail.com>
2023-02-16 11:32:00 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,589 @@
+// var stringVar = "jaffar10@gmail.com"
+
+// var numberVar = 20;   
+
+// var isActive = true  
+
+// alert(isActive)
+
+
+
+// 80 && 100 == A +
+// 70 && 79 == A
+// 60 && 69 == B
+// 50 && 59 == C
+// 40 && 49 == D
+// 0 && 39 == Fail
+
+// var number1 = +prompt("ENTER NUMBER 1")
+//  var number2 = +prompt("ENTER NUMBER 2")
+//  var opt = prompt("ENTER opt")
+
+
+//  if (opt === "+") {
+//      console.log(number1 + number2)
+//  } else if (opt === "-") {
+//      console.log(number1 - number2)
+//  } else if (opt === "*") {
+//      console.log(number1 * number2)
+//  } else if (opt === "/") {
+//      console.log(number1 / number2)
+//  }
+
+
+                        //    chapter:no1
+    //Question-1//
+    // alert ("Hello world!");
+
+    // Question-2
+    // alert("This is an alert message box.");  // display string message
+
+    // Question-3
+    // alert("Line 1\nLine 2");
+
+    // Question-4
+    // alert (" Hello World \n Welcome to the javaTpoint.com \n This is an alert dialog box ");  
+
+    // Question-5
+    // console.log("Hello, world!");
+ 
+                            // chapter:no2
+   // Question-1
+//    var userName
+
+   // Question-2
+//    var username = "zaviyar";
+
+    // Question-3
+    // console.log("Hello, " + name + "!");
+
+    // Question-4
+    // const isMember = false;
+    // alert(isMember);
+
+    // Question-5
+    // var message = "zaviyar";
+
+    // alert(message);
+
+    // Question-6
+// var book = "A Smarter Way to Learn JavaScript";
+
+// alert("I am reading " + book + " these days.");
+
+            //  chapter:3
+
+// Question-1
+// var age = 25;
+
+// alert("I am " + age + " years old.");
+
+
+  // Question-2
+// var visits = 0;
+
+// visits = visits + 1;
+
+// document.write("You have visited this site " + visits + " time(s).");
+
+      // Question-3
+//       var birthYear = 2000;
+
+// document.write("My birth year is " + birthYear + ".");
+
+//    chapter-5
+
+    //    Question-1
+//     var num1 = 5;
+//     var num2 = 10;
+//     var result = num1 + num2;
+
+//    document.write("The sum of " + num1 + " and " + num2 + " is " + result + ".");
+
+      //    Question-2
+//       var num1 = 20;
+// var num2 = 10;
+
+// var subtraction = num1 - num2;
+// var multiplication = num1 * num2;
+// var division = num1 / num2;
+// var modulus = num1 % num2;
+
+// document.write("The subtraction of " + num1 + " and " + num2 + " is " + subtraction + "." + "<br>");
+// document.write("The multiplication of " + num1 + " and " + num2 + " is " + multiplication + "." + "<br>");
+// document.write("The division of " + num1 + " and " + num2 + " is " + division + "." + "<br>");
+// document.write("The modulus of " + num1 + " and " + num2 + " is " + modulus + "." + "<br>");
+
+       //    Question-3
+
+    //    Addition
+//     var num1 = 10;
+//     var num2 = 20;
+//     var result = num1 + num2;
+    
+//       Subtraction:
+// var num1 = 10;
+// var num2 = 20;
+// var result = num1 - num2;
+
+// Multiplication:
+// var num1 = 10;
+// var num2 = 20;
+// var result = num1 * num2;
+
+// Division:
+// var num1 = 10;
+// var num2 = 20;
+// var result = num1 / num2;
+ 
+// Modulus:
+// var num1 = 10;
+// var num2 = 20;
+// var result = num1 % num2;
+
+// Increment
+// var num = 10;
+// num++;
+
+// Decrement
+// var num = 10;
+// num--;
+
+ 
+// Question-4
+// var ticketPrice = 600;
+
+// document.write("The cost of one movie ticket is " + ticketPrice + " PKR.");
+
+   // Question-5
+//     var num = 5;
+
+//  document.write("<h3>Multiplication Table of " + num + "");
+
+//  for (var i = 1; i <= 10; i++) {
+//   document.write(num + " x " + i + " = " + num * i + "");
+//  }
+
+
+// var num = 5;
+
+// document.write("<h3>Multiplication Table of " + num + "</h3>");
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(num + " x " + i + " = " + num * i + "<br>");
+// }
+
+      // Question-6
+// var tempCelsius = 30;
+// var tempFahrenheit = (tempCelsius * 9/5) + 32;
+
+// document.write(tempCelsius + "°C is equal to " + tempFahrenheit + "°F" + "<br>");
+
+// var tempFahrenheit = 86;
+// var tempCelsius = (tempFahrenheit - 32) * 5/9;
+
+// document.write(tempFahrenheit + "°F is equal to " + tempCelsius + "°C");
+
+       // Question-7
+       // Item details
+// var itemName1 = "Shirt";
+// var itemPrice1 = 1000;
+// var itemQuantity1 = 2;
+
+// var itemName2 = "Pants";
+// var itemPrice2 = 1500;
+// var itemQuantity2 = 1;
+
+// // Calculate total cost
+// var subTotal = (itemPrice1 * itemQuantity1) + (itemPrice2 * itemQuantity2);
+// var shippingCost = 200;
+// var totalCost = subTotal + shippingCost;
+
+// // Display checkout details
+// document.write("<h2>Checkout</h2>");
+
+// document.write("<p>Item 1: " + itemName1 + "</p>");
+// document.write("<p>Price: " + itemPrice1 + "</p>");
+// document.write("<p>Quantity: " + itemQuantity1 + "</p>");
+// document.write("<br>");
+
+// document.write("<p>Item 2: " + itemName2 + "</p>");
+// document.write("<p>Price: " + itemPrice2 + "</p>");
+// document.write("<p>Quantity: " + itemQuantity2 + "</p>");
+// document.write("<br>");
+
+// document.write("<p>Subtotal: " + subTotal + "</p>");
+// document.write("<p>Shipping: " + shippingCost + "</p>");
+// document.write("<p>Total: " + totalCost + "</p>");
+
+        // Question-8
+//         var totalMarks = 500;
+// var marksObtained = 375;
+
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// document.write("<h2>Percentage Calculator</h2>");
+// document.write("<p>Total Marks: " + totalMarks + "</p>");
+// document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+// document.write("<p>Percentage: " + percentage + "%</p>");
+
+        // Question-10
+//         num = 5
+// num += 2  # add 2 to num, num = 7
+// num *= 3  # multiply num by 3, num = 21
+// num -= 4  # subtract 4 from num, num = 17
+// num //= 5  # integer division of num by 5, num = 3
+// num **= 2  # raise num to the power of 2, num = 9
+// print(num)  # print the final value of num
+
+          // Question-11
+        //   function calculateAge(birthdate) {
+        //     const now = new Date();
+        //     const birthdateObj = new Date(birthdate);
+        //     let years = now.getFullYear() - birthdateObj.getFullYear();
+        //     let months = now.getMonth() - birthdateObj.getMonth();
+        //     let days = now.getDate() - birthdateObj.getDate();
+          
+        //     // If the birthdate is later in the current month, subtract a month
+        //     if (months < 0 || (months === 0 && days < 0)) {
+        //       years--;
+        //       months += 12;
+        //     }
+          
+        //     // If the birthdate is later in the current day, subtract a day
+        //     if (days < 0) {
+        //       const daysInMonth = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+        //       days += daysInMonth;
+        //       months--;
+        //     }
+          
+        //     return {
+        //       years: years,
+        //       months: months,
+        //       days: days
+        //     };
+        //   }
+          
+               //    chapter-6
+
+               // Question-1
+//                // prompt the user to enter a number and store it in a variable
+// let num = prompt("Enter a number:");
+
+// // convert the input to a number
+// num = parseInt(num);
+
+// // perform arithmetic operations
+// let double = num * 2;
+// let triple = num * 3;
+// let square = num ** 2;
+// let cube = num ** 3;
+// let squareRoot = Math.sqrt(num);
+
+// // create a string to display the results
+// let result = "The number is: " + num + "<br>";
+// result += "Double of the number is: " + double + "<br>";
+// result += "Triple of the number is: " + triple + "<br>";
+// result += "Square of the number is: " + square + "<br>";
+// result += "Cube of the number is: " + cube + "<br>";
+// result += "Square root of the number is: " + squareRoot;
+
+// // display the results in the browser
+// document.write(result);
+      
+      // Question-3
+//       const name = prompt("What is your name?");  // prompt the user for their name
+// alert(`Hello, ${name}! Welcome to our website.`);  // greet the user with their name
+
+      // Question-4
+// function displayTable() {
+//     const numberInput = document.getElementById("number");
+//     const number = numberInput.value ? parseInt(numberInput.value) : 5;
+//     const table = document.getElementById("table");
+
+//     // Clear any existing rows from the table
+//     while (table.rows.length > 0) {
+//       table.deleteRow(0);
+//     }
+
+//     // Add a new row for each multiple of the number
+//     for (let i = 1; i <= 10; i++) {
+//       const row = table.insertRow();
+//       const cell1 = row.insertCell();
+//       const cell2 = row.insertCell();
+//       cell1.innerText = i;
+//       cell2.innerText = i * number;
+//     }
+             
+            
+      // Question-5
+//       const subject1 = prompt("Enter the name of the first subject:");
+// const subject2 = prompt("Enter the name of the second subject:");
+// const subject3 = prompt("Enter the name of the third subject:");
+
+// console.log(`The three subjects are ${subject1}, ${subject2}, and ${subject3}.`);
+//         ------------b-------------
+//         const subject1 = prompt("Enter the name of the first subject:");
+// const subject2 = prompt("Enter the name of the second subject:");
+// const subject3 = prompt("Enter the name of the third subject:");
+
+// const totalMarks = 100;
+
+// console.log(`The three subjects are ${subject1}, ${subject2}, and ${subject3}.`);
+// console.log(`The total marks for each subject is ${totalMarks}.`);
+        //   -----------c----------
+        //   const subject1 = prompt("Enter the name of the first subject:");
+// const totalMarks = 100;
+
+// const marks1 = prompt(`Enter the obtained marks for ${subject1}:`);
+
+// console.log(`Obtained marks for ${subject1}: ${marks1} out of ${totalMarks}`);
+
+
+                 //    chapter-7
+                 // Question-1
+//                  const cityName = prompt("Enter a city name:");
+
+// if (cityName === "Karachi") {
+//   console.log("Welcome to city of lights!");
+// }
+
+                  //  Question-2
+                  // Generate a random number between 1 and 10
+// const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// // Prompt the user to enter a number
+// const userGuess = parseInt(prompt("Guess a number between 1 and 10"));
+
+// // Check if the user's guess matches the random number
+// if (userGuess === randomNumber) {
+//   console.log("Bingo! Correct answer");
+// } else {
+//   console.log("Wrong answer. The correct number was " + randomNumber);
+// }
+               
+                 //  Question-3
+//                  // Prompt the user to enter a number
+// const number = parseInt(prompt("Enter a number:"));
+
+// // Check if the number is divisible by 3
+// if (number % 3 === 0) {
+//   console.log(number + " is divisible by 3");
+// } else {
+//   console.log(number + " is not divisible by 3");
+// }
+     
+            //  Question-4
+    // Prompt the user to enter a number
+// const number = parseInt(prompt("Enter a number:"));
+
+// // Check if the number is even or odd
+// if (number % 2 === 0) {
+//   console.log(number + " is an even number");
+// } else {
+//   console.log(number + " is an odd number");
+// }
+           
+           //  Question-5
+       // Prompt the user to enter the current temperature
+// const temperature = parseInt(prompt("Enter the current temperature:"));
+
+// // Check if the temperature is above 40 degrees
+// if (temperature > 40) {
+//   console.log("It is too hot outside");
+// } else {
+//   console.log("The temperature is comfortable");
+// }
+    
+                 //  Question-6
+//           // Prompt the user to enter the current temperature
+// const temperature = parseInt(prompt("Enter the current temperature:"));
+
+// // Check if the temperature is above 30 degrees
+// if (temperature > 30) {
+//   console.log("The Weather today is Normal");
+// } else {
+//   console.log("The temperature is comfortable");
+// }
+        
+                 //  Question-7
+//        // Prompt the user to enter the current temperature
+// const temperature = parseInt(prompt("Enter the current temperature:"));
+
+// // Check if the temperature is above 20 degrees
+// if (temperature > 20) {
+//   console.log("Today's Weather is cool");
+// } else {
+//   console.log("The temperature is comfortable");
+// }
+          
+            //  Question-8
+//         // Prompt the user to enter two numbers and an operator
+// const num1 = parseFloat(prompt("Enter the first number:"));
+// const num2 = parseFloat(prompt("Enter the second number:"));
+// const operator = prompt("Enter an operator (+, -, *, /, %):");
+
+// // Perform the appropriate operation based on the operator
+// if (operator === "+") {
+//   console.log(num1 + " + " + num2 + " = " + (num1 + num2));
+// } else if (operator === "-") {
+//   console.log(num1 + " - " + num2 + " = " + (num1 - num2));
+// } else if (operator === "*") {
+//   console.log(num1 + " * " + num2 + " = " + (num1 * num2));
+// } else if (operator === "/") {
+//   console.log(num1 + " / " + num2 + " = " + (num1 / num2));
+// } else if (operator === "%") {
+//   console.log(num1 + " % " + num2 + " = " + (num1 % num2));
+// } else {
+//   console.log("Invalid operator");
+// }
+      //     chapter-12
+     //  Question-1
+    // Prompt the user to enter a character
+// const inputChar = prompt("Enter a character:");
+
+// // Get the ASCII code of the input character
+// const asciiCode = inputChar.charCodeAt(0);
+
+// // Check whether the input is a number, uppercase letter, or lowercase letter
+// if (asciiCode >= 48 && asciiCode <= 57) {
+//   console.log(inputChar + " is a number");
+// } else if (asciiCode >= 65 && asciiCode <= 90) {
+//   console.log(inputChar + " is an uppercase letter");
+// } else if (asciiCode >= 97 && asciiCode <= 122) {
+//   console.log(inputChar + " is a lowercase letter");
+// } else {
+//   console.log("Invalid input");
+// }
+
+            //  Question-2
+      // Prompt the user to enter two integers
+// const num1 = parseInt(prompt("Enter the first integer:"));
+// const num2 = parseInt(prompt("Enter the second integer:"));
+
+// // Check if the two integers are equal
+// if (num1 === num2) {
+//   console.log("The two integers are equal");
+// } else {
+//   // Find the larger of the two integers
+//   const largerNum = (num1 > num2) ? num1 : num2;
+//   console.log("The larger integer is " + largerNum);
+// }
+      
+              //  Question-3
+//      // Prompt the user to enter a number
+// const num = parseInt(prompt("Enter a number:"));
+
+// // Check whether the number is positive, negative, or zero
+// if (num > 0) {
+//   console.log(num + " is a positive number");
+// } else if (num < 0) {
+//   console.log(num + " is a negative number");
+// } else {
+//   console.log(num + " is zero");
+// }
+         
+              //  Question-4
+     // Prompt the user to enter a character
+// const char = prompt("Enter a character:");
+
+// // Check whether the character is a vowel
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+//     char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+//   console.log(char + " is a vowel");
+// } else {
+//   console.log(char + " is not a vowel");
+// }
+         
+              //  Question-5
+//          // Store the correct password in a variable
+// const correctPassword = "mysecretpassword";
+
+// // Use the password variable in your program as needed
+     
+           //  Question-6
+//       // Store the correct password in a variable
+// const correctPassword = "mysecretpassword";
+
+// // Prompt the user to enter their password
+// const enteredPassword = prompt("Enter your password:");
+
+// // Check if the entered password matches the correct password
+// if (enteredPassword === correctPassword) {
+//   console.log("Password is correct");
+// } else {
+//   console.log("Password is incorrect");
+// }
+
+                  //  Question-7
+//              // Store the correct password in a variable
+// const correctPassword = "mysecretpassword";
+
+// // Prompt the user to enter their password
+// const enteredPassword = prompt("Enter your password:");
+
+// // Check if the entered password is empty
+// if (!enteredPassword) {
+//   console.log("Please enter your password");
+// } else {
+//   // Check if the entered password matches the correct password
+//   if (enteredPassword === correctPassword) {
+//     console.log("Password is correct");
+//   } else {
+//     console.log("Password is incorrect");
+//   }
+// }
+       
+               //  Question-8
+//           // Store the correct password in a variable
+// const correctPassword = "mysecretpassword";
+
+// // Prompt the user to enter their password
+// const enteredPassword1 = prompt("Enter your password:");
+// const enteredPassword2 = prompt("Enter your password again:");
+
+// // Check if the entered passwords match
+// if (enteredPassword1 === enteredPassword2) {
+//   if (enteredPassword1 === correctPassword) {
+//     console.log("Correct! The password you entered matches the original password");
+//   } else {
+//     console.log("Incorrect password");
+//   }
+// } else {
+//   console.log("The passwords do not match");
+// }
+     
+               //  Question-9
+//        var hour = new Date().getHours();
+// var greeting;
+
+// if (hour < 12) {
+//   greeting = "Good morning!";
+// } else if (hour < 18) {
+//   greeting = "Good afternoon!";
+// } else {
+//   greeting = "Good evening!";
+// }
+
+// console.log(greeting);
+        
+               //  Question-10
+               
+//      // Ask the user to enter the time in 24-hour clock format
+// var time = prompt("Enter the time in 24-hour clock format (e.g. 1900)");
+
+// // Extract the hours and minutes from the time string
+// var hours = parseInt(time.substr(0, 2));
+// var minutes = parseInt(time.substr(2, 2));
+
+// // Check if the time is in the morning or afternoon/evening
+// if (hours >= 0 && hours < 12) {
+//   console.log("The time is " + hours + ":" + minutes + "am");
+// } else if (hours == 12) {
+//   console.log("The time is " + hours + ":" + minutes + "pm");
+// } else if (hours > 12 && hours <= 23) {
+//   var pmHours = hours - 12;
+//   console.log("The time is " + pmHours + ":" + minutes + "pm");
+// } else {
+//   console.log("Invalid time entered");
+// }
